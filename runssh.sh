151bbe0e88edc7dbac5bf6b626502de3a7704258
shabbyboy <925750301@qq.com>
2019-04-04 17:53:49 +0800

    修改点私人的东西

--- runssh.sh
+++ runssh.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# 通过ssh 嵌套 expect 
 expect -c "
-	set password 314159;
+	set password ******;
 	set arg $1;
 	if { \"$arg\" == \"T\" } {
 		 spawn ssh-add .ssh/id_rsa.xuxiaolong
 		 expect \"*xuxiaolong:\" { send \"$password\r\" }
 		 expect eof
 	};
-	spawn ssh -i ~/.ssh/id_rsa.xuxiaolong -A xuxiaolong#UTF-8@192.168.10.5 -p 222;
+	spawn ssh -i ~/.ssh/id_rsa.xuxiaolong -A xuxiaolong#UTF-8@ip -p 222;
 	expect \"xuxiaolong:\" { send \"$password\r\" };
 	interact
 "
